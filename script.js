9efe9f6ec0dba960b401de5ec5c210cc5767ebb3
rypark91 <59321464+rypark91@users.noreply.github.com>
2023-10-31 19:35:15 -0600

    file upload
    Initial project upload.

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+function reveal() {
+    var reveals = document.querySelectorAll(".letter");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
+//reveal();  
+
+/**
+ * code source: https://alvarotrigo.com/blog/css-animations-scroll/
+ */
